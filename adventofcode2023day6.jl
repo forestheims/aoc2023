28f84af419b8448b01b915ed1200d4269eeedf9a
forestheims <forestheims@gmail.com>
2023-12-11 10:32:17 +1100

    add input for past 5 days.

---
+++ adventofcode2023day6.jl
@@ -0,0 +1,14 @@
+### A Pluto.jl notebook ###
+# v0.19.32
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0010dfb0-9714-11ee-014f-5135ccfa5fdc
+begin
+	time = [60, 94, 78, 82]
+	distance = [475, 2138, 1015, 1650]
+end
+
+# ╔═╡ Cell order:
+# ╠═0010dfb0-9714-11ee-014f-5135ccfa5fdc
